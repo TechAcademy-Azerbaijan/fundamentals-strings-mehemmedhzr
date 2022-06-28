bb824d07e3c463d8f6293ec9a3bb7993ffdd8942
mehemmedhzr <105041148+mehemmedhzr@users.noreply.github.com>
2022-06-28 00:13:44 -0700

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,9 +1,27 @@
+const { logger } = require('prompt');
 const prompt = require('prompt');
 prompt.start();
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+prompt.get('input', function (err, result){
+    let verilen = result.input
+    let twocount = 0
+    let fivecount = 0
+    for(let i = 0; i < verilen.length; i++){
+        if(parseInt(verilen[i]) === 2){
+            twocount = twocount + 1
+        }
+        if(parseInt(verilen[i]) === 5){
+            fivecount = fivecount + 1
+        }
+    }
+    if(twocount > fivecount){
+        console.log(2);
+    }
+    else if(fivecount > twocount){
+        console.log(5);
+    }
+    else{
+        console.log('=');
+    }
+})
