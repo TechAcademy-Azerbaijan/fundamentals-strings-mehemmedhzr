b42b31e27f5e348785f722c90e01fc733641b5c6
mehemmedhzr <105041148+mehemmedhzr@users.noreply.github.com>
2022-06-28 00:25:08 -0700

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -1,9 +1,15 @@
+const { logger } = require('prompt');
 const prompt = require('prompt');
 prompt.start();
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+prompt.get('input', function (err, result){
+    let str = result.input
+    let sentence = 0
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === '!' || str[i] === '.'){
+            sentence += 1
+        }
+    }
+    console.log(sentence);
+})
