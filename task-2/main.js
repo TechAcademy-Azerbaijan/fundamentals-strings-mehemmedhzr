718e79f77de73a9121d2b98e0af521a7b5ce026a
mehemmedhzr <105041148+mehemmedhzr@users.noreply.github.com>
2022-06-27 23:51:04 -0700

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -1,9 +1,19 @@
+const { logger } = require('prompt');
 const prompt = require('prompt');
 prompt.start();
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+prompt.get('input', function (err, result){
+    let verilen = result.input.split(',')
+    let first = parseInt(verilen/100)
+    let last = parseInt(verilen%10)
+    if(first > last){
+        console.log(first);
+    }
+    else if(last > first){
+        console.log(last);
+    }
+    else{
+        console.log('=');
+    }
+})
