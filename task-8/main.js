2dba047da98d9f4b9aeb614bc3eb486c739dd82c
mehemmedhzr <105041148+mehemmedhzr@users.noreply.github.com>
2022-06-28 02:03:15 -0700

    Update main.js

--- task-8/main.js
+++ task-8/main.js
@@ -1,9 +1,13 @@
+const { logger } = require('prompt');
 const prompt = require('prompt');
 prompt.start();
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+prompt.get('input', function (err, result){          // introduction to algorithms
+    let str = result.input   
+    let str1 = str.split(' ')
+    for(let i = 0; i < str1.length; i++){
+        str1[i] = str1[i].charAt(0).toUpperCase() + str1[i].slice(1);
+    }
+    console.log(str1.join(' '));
+})
