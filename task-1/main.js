49c1bb1b3b375717cd5c00633b5c1de262cfa02e
mehemmedhzr <105041148+mehemmedhzr@users.noreply.github.com>
2022-06-27 23:39:39 -0700

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -2,8 +2,10 @@ const prompt = require('prompt');
 prompt.start();
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+prompt.get('input', function (err, result){
+  let verilen = result.input.split(',')
+  let birinci = parseInt(verilen[0])
+  let ikinci = parseInt(verilen[1])
+  let ucuncu = parseInt(verilen[2])
+  console.log(Math.min(birinci,ikinci,ucuncu));  
+})
