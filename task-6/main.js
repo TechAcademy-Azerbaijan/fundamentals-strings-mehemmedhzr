aac4299a847b9a1eaf80f308cfe2f10687779df1
mehemmedhzr <105041148+mehemmedhzr@users.noreply.github.com>
2022-07-02 04:46:00 -0700

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -3,7 +3,13 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+let a = result.input;
+let arr = a.split(" ");
+let count = 0;
+for(let i = 0; i < arr.length; i++){
+    if(arr[i] !== "" && arr[i] !== "!" && arr[i] !== "," && arr[i] !== "."){
+        count++
+    }
+}
+console.log(count);
 });
