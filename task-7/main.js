b1facae68259f6bbe8da897dfd4ea1e2a061288d
mehemmedhzr <105041148+mehemmedhzr@users.noreply.github.com>
2022-07-02 05:23:50 -0700

    Update main.js

--- task-7/main.js
+++ task-7/main.js
@@ -3,7 +3,13 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+let a = result.input;
+let str = a.split("p");
+let count = 0;
+for(let i = 0; i < str.length; i++){
+    if(count < str[i].length){
+        count = str[i].length
+    }
+}
+console.log(count);
 });
